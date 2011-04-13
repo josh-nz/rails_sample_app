6a93eed428b2db2117c389a57cce0786995d11e0
josh.nz <josh.nz@gmail.com>
2011-04-13 23:34:26 +1200

    Added page security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-    @title = "Sign up"
-  end
-
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
   end
   
+  def new
+    @user = User.new
+    @title = "Sign up"
+  end
+   
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -22,4 +25,29 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
 end
