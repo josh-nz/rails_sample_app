5f8088a64f9010f111988a8a15b752f9e4faee92
josh.nz <josh.nz@gmail.com>
2011-04-22 22:41:59 +1200

    Added followers and followed pages; updated feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -20,4 +20,9 @@ class Micropost < ActiveRecord::Base
   
   #default_scope :order => 'microposts.created_at DESC'
   default_scope order('microposts.created_at DESC')
+  
+  def self.from_users_followed_by(user)
+    followed_ids = user.following.map(&:id).join(", ")
+    where("user_id IN (#{followed_ids}) OR user_id = ?", user)
+  end
 end
